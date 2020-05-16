212a5d5b4fa172586607177e6030db50da353e1e
theandex <46944138+theandex@users.noreply.github.com>
2020-05-15 22:56:11 -0400

    portafolio

---
+++ src/app/pages/item/item.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ProductosService } from '../../services/productos.service';
+import { ProductoDescripcion } from 'src/app/interfaces/producto-descripcion.interfaces';
+
+@Component({
+  selector: 'app-item',
+  templateUrl: './item.component.html',
+  styleUrls: ['./item.component.css']
+})
+export class ItemComponent implements OnInit {
+
+  producto: ProductoDescripcion;
+  id: string;
+
+  constructor( private route: ActivatedRoute, public productoService: ProductosService ) { }
+
+  ngOnInit(): void {
+
+    this.route.params.subscribe( parametros =>{
+
+      this.productoService.getProducto(parametros.id).subscribe( (producto: ProductoDescripcion) =>{
+        this.id = parametros.id;
+        this.producto = producto;
+      });
+
+    });
+
+  }
+
+}
